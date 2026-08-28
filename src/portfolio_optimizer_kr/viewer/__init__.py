42ba2c9ef8416025a0addc24f36e7821ba34c092
JY <82421088+comus93@users.noreply.github.com>
2026-08-29 07:45:02 +0900

    feat: expose interactive report viewer API

--- src/portfolio_optimizer_kr/viewer/__init__.py
+++ src/portfolio_optimizer_kr/viewer/__init__.py
@@ -1,3 +1,14 @@
+from .builder import build_report_model, build_report_model_from_artifacts
 from .loader import RunArtifacts, load_run_artifacts
+from .renderer import generate_report, render_report
+from .report_model import ReportModel
 
-__all__ = ["RunArtifacts", "load_run_artifacts"]
+__all__ = [
+    "RunArtifacts",
+    "ReportModel",
+    "build_report_model",
+    "build_report_model_from_artifacts",
+    "generate_report",
+    "load_run_artifacts",
+    "render_report",
+]
